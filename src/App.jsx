960623d79a6416458045f3bdd4770eb81a5e7cf2
Divyanshu Verma <divyanshudverma@gmail.com>
2024-05-12 16:00:37 +0530

    DV

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import Home from './Pages/Home';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+  <div className='App'> 
+    <Home />
+     </div>    
+    </>
+  );
+}
+
+export default App
